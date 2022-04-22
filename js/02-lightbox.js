d2a3a8608a51bd90916e8894542c46099bb14bb6
bragynator <92689909+bragynator@users.noreply.github.com>
2022-04-22 11:46:04 +0300

    Second task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.innerHTML = galleryMarkup;
+
+function createGalleryMarkup(items) {
+	return items
+		.map(
+			item => `
+                <a class="gallery__item" href="${item.original}">
+                    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+                </a>`,
+		)
+		.join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {
+	showCounter: false,
+	scaleImageToRatio: true,
+	maxZoom: 4,
+	captionsData: 'alt',
+	captionDelay: 250,
+});
+
+gallery.on();
